97f8921bc76aff648049a06294886550ff00719a
Rebecca <rbjanis@gmail.com>
2020-01-09 22:06:43 -0500

    Week 1

---
+++ Week1/Australian-fires.R
@@ -0,0 +1,24 @@
+# Week 1: Australian Fires
+
+require(tidyverse)
+theme_set(theme_classic())
+fires <- tidytuesdayR::tt_load(2020, week = 2)
+
+rainfall <-  fires$rainfall
+temp <- fires$temperature
+
+temp <- mutate(temp, year_month = format(date, "%Y-%m"),
+               year = as.numeric(format(date, "%Y")))
+
+filter(temp, temp_type == "max") %>%
+  filter(year >= 1919) %>%
+  group_by(year, city_name) %>%
+  summarize(temperature = mean(temperature, na.rm = T)) %>%
+  ggplot(aes(x = year, y = temperature, group = city_name, color = city_name)) +
+  geom_line() +
+  geom_smooth(method = "lm", se = F, size = 1.2) +
+  scale_x_continuous(name = "Year", breaks = seq(1910, 2020, 10)) +
+  scale_y_continuous(name = "High Temperature (Celsius)", breaks = seq(14, 30, 2)) +
+  scale_color_discrete(name = "City") +
+  ggtitle("Average daily high temperatures in Australian cites have increased over the past 100 years")
+ggsave("Week1/high-temperatures.png")
